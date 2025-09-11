8689ff2b7e65e24ebba913eaeffedb9367087470
GiftMHB <giftmohuba159@gmail.com>
2025-09-11 13:06:12 +0200

    Index page

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+// Dynamic Greeting
+const greeting = document.getElementById("dynamic-greeting");
+const hour = new Date().getHours();
+if (hour < 12) greeting.textContent = "☀️ Good Morning!";
+else if (hour < 18) greeting.textContent = "🌤️ Good Afternoon!";
+else greeting.textContent = "🌙 Good Evening!";
