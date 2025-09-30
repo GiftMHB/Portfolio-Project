a58c6ecffddda72e07edfdc2d1cd6a4aa75cadb0
GiftMHB <giftmohuba159@gmail.com>
2025-09-30 20:44:29 +0200

    Updates

--- js/skills.js
+++ js/skills.js
@@ -20,7 +20,7 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 
-  setTimeout(showSlides, 5000); 
+  setTimeout(showSlides, 3000); 
 }
 
 function plusSlides(n) {
